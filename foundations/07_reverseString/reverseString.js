20a5c14bbd7fd79a9184f227c3da52f3a92289ee
Torolero <42136673+Torolero@users.noreply.github.com>
2026-03-20 16:18:56 -0700

    Reverse all possible strings provided to it
    Will convert strings into arrays. Then it will take
    the arrays and reverse them. Then it will join the
    array back into a string.

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let originalArr = string.split("");
+    let newArr = originalArr.reverse();
+    let newString = newArr.join("");
 
+    return newString;
 };
+
+reverseString("");
 
 // Do not edit below this line
 module.exports = reverseString;
