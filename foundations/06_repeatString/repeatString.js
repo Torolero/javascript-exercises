dfa719ea06b00b9265a7074dbc1d33915256366e
Torolero <42136673+Torolero@users.noreply.github.com>
2026-03-20 16:00:28 -0700

    Fix string repetition
    Used while loop to repeat a string a given number
    of times.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let count = 0;
+    let repeatedString = "";
+    if (num < 0) {
+        return "ERROR";
+    }
+    while (count < num) {
+        repeatedString += string;
+        count++;
+    }
+    return repeatedString;
 };
+
+repeatString("hey", -5);
 
 // Do not edit below this line
 module.exports = repeatString;
