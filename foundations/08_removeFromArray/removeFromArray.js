6fca83f4a4a5ab8c50ff24228c3beec143bf4289
Torolero <42136673+Torolero@users.noreply.github.com>
2026-03-23 09:17:38 -0700

    Save progress on current exercise
    Initialized var and started pseudocode.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, input) {
+/*
+Function should take an array and another argument.
+Function should remove the 'other argument' from the array if present.
+Function should return the updated results.
+*/
+    let newArray = array;
+
+
 };
+
+removeFromArray([1, 2, 3, 4], 3);
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
