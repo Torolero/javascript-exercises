310a943108eec1509fd6b6ff08ff40590215ae19
Torolero <42136673+Torolero@users.noreply.github.com>
2026-03-24 23:12:42 -0700

    Fix all issues in the file
    Used filter() method to iterate through the array
    and return new array based upon predetermined values.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,15 +1,15 @@
-const removeFromArray = function(array, input) {
+const removeFromArray = function(array, input, inputTwo, inputThree, inputFour) {
 /*
 Function should take an array and another argument.
 Function should remove the 'other argument' from the array if present.
 Function should return the updated results.
 */
-    let newArray = array;
-
+    let newArray = array.filter(num => num !== input && num !== inputTwo && num !== inputThree && num !== inputFour);
+    return newArray;
 
 };
 
-removeFromArray([1, 2, 3, 4], 3);
+removeFromArray([1, 2, 3], "1", 3);
 
 
 // Do not edit below this line
