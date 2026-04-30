1b61dbe7646e02df6d81cfb2de2958de2e6ac743
Torolero <42136673+Torolero@users.noreply.github.com>
2026-04-30 15:44:36 -0700

    Complete exercise 9 with all tests passed
    I used a if statement with while loops to check for
    inputs we don't want to accept and move forward with
    the ones that we do like.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(intOne, intTwo) {
+    let total = 0;
+    if (intOne > intTwo) {
+       while (intOne >= intTwo) {
+        total += intOne;
+        intOne--;
+  
+    } 
+    } else if (intOne < 0 || intOne % 1 !== 0 || typeof intTwo !== 'number' ) {
+        return "ERROR";
+    }else {
+        while (intOne <= intTwo) {
+        total += intOne;
+        intOne++;
+    }
+    }
+    
 
+    return total;
 };
+sumAll(10, [90, 1]);
 
 // Do not edit below this line
 module.exports = sumAll;
