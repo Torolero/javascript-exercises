54c21d53d0479f4a010bc81dea9111e486cd7166
Torolero <42136673+Torolero@users.noreply.github.com>
2026-04-30 16:44:26 -0700

    Calculate leap years through division & remainder
    Added logic to calculate weather a year was a leap
    year.

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
+const leapYears = function(inputYear) {
+/*
+If year is divisible by 4 = leap year
+If year is divisible by 100 != leap year
+    Unless it is also divisible by 400
+*/
+    if (inputYear % 4 == 0 && inputYear % 100 !== 0 || inputYear % 400 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 
+    return;
 };
+leapYears(700); // is a leap year: returns true
+//leapYears(1985); // is not a leap year: returns false
 
 // Do not edit below this line
 module.exports = leapYears;
