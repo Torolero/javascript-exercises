a4898997df85269f5b9c6c8e7829487311c418cc
Torolero <42136673+Torolero@users.noreply.github.com>
2026-05-12 15:58:20 -0700

    Convert temperature scales using formula
    Used formula found online to convert C to F & vise
    versa.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(numberC) {
+  //Take input of F number
+  //Convert to C using formula
+  //return
+  let celsius = ((numberC - 32) * 5 / 9);
+  return Math.ceil(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(numberF) {
+  //Take input of C number
+  let farenheit = ((numberF * 9 / 5 + 32));
+  return Math.ceil(farenheit * 10) / 10;
 };
+
+convertToCelsius(32);
+convertToFahrenheit(0);
+
+
 
 // Do not edit below this line
 module.exports = {
